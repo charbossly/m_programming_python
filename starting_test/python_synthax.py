ed7f9752e7de8335997235a2b238ba810c109173
charbossly <assogbacharbossly@gmail.com>
2022-11-29 19:55:14 +0100

    start python:variables_synthax

---
+++ starting_test/python_synthax.py
@@ -0,0 +1,7 @@
+x = 1+2\
++6
+print(x)
+
+name= 'charbel'
+if(name=='charbel'):
+    print('right name')
---
+++ starting_test/python_variables.py
@@ -0,0 +1,21 @@
+a=b=c=10
+d,e,f= 11,10,9
+print(a,b,c)
+print(d,e,f)
+x = 5
+print ( type(x)) #to get type of variable
+del x
+#print(x)
+
+#list 
+list = [1,2,3]
+print(list)
+list[0] = 12
+print(list)
+
+#tuple
+tuple= ('a','b',12)
+print(tuple)
+# tuple[1] = 'c'
+# print(tuple)
+#TypeError: 'tuple' object does not support item assignment
